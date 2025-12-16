2d5df0613799c9036807484f28df6e924679cf98
sean.kim <51458279+gony0811@users.noreply.github.com>
2025-12-16 14:59:46 +0900

    Position Create

--- src/HCB.UI/MAIN UI/ViewModels/USub06ViewModel.cs
+++ src/HCB.UI/MAIN UI/ViewModels/USub06ViewModel.cs
@@ -2,7 +2,10 @@
 using CommunityToolkit.Mvvm.Input;
 using HCB.Data.Entity;
 using HCB.Data.Entity.Type;
+using HCB.Data.Repository;
 using HCB.IoC;
+using SharpDX;
+using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
@@ -16,14 +19,17 @@ namespace HCB.UI
     {
         private readonly DeviceManager deviceManager;
         private readonly DialogService dialogService;
+        private readonly MotionPositionRepository positionRepository;
+
         [ObservableProperty]
         private ObservableCollection<IAxis> motionList = new();
         [ObservableProperty] private IAxis selectedMotion;
 
-        public USub06ViewModel(DeviceManager deviceManager, DialogService dialogService)
+        public USub06ViewModel(DeviceManager deviceManager, DialogService dialogService, MotionPositionRepository positionRepository)
         {
             this.deviceManager = deviceManager;
             this.dialogService = dialogService;
+            this.positionRepository = positionRepository;
 
             var motionDevices = deviceManager.Devices
                 .OfType<IMotionDevice>()
@@ -42,25 +48,51 @@ namespace HCB.UI
         [RelayCommand]
         public async Task PositionCreate()
         {
-            if(SelectedMotion != null) dialogService.ShowMessage("경고", "모션을 선택해주세요");
+            if(SelectedMotion == null) dialogService.ShowMessage("경고", "모션을 선택해주세요");
 
-            var name = new NameViewModel();
-            bool? step1 = await dialogService.ShowEditDialog(name);
+            var position = new DMotionPosition();
 
-            if (step1 == false) return;
+            bool? result = await dialogService.ShowEditDialog(position);
+            if (result == false) return;
 
-            double? result = dialogService.ShowEditNumDialog(0, SelectedMotion.LimitMinPosition, SelectedMotion.LimitMaxPosition);
-
-            if (result == null) return;
-
-            new MotionPosition
+            
+            try
             {
-                Name = name.Name,
-                Position = result.Value,
-                Speed = 
-            };
+                var entity = new MotionPosition()
+                {
+                    MotionId = SelectedMotion.Id,
+                    Name = position.Name,
+                    Position = position.Position,
+                    Speed = position.Speed
+                };
 
-            SelectedMotion.PositionList.Add();
+                entity = await positionRepository.AddAsync(entity);
+                position.Id = entity.Id;
+                SelectedMotion.PositionList.Add(position);
+            }
+            catch(Exception e)
+            {
+                dialogService.ShowMessage("오류", $"저장중 에러 발생:\n {e}");
+            }
+            
+
+
+            //bool? step1 = await dialogService.ShowEditDialog(name);
+
+            //if (step1 == false) return;
+
+            //double? result = dialogService.ShowEditNumDialog(0, SelectedMotion.LimitMinPosition, SelectedMotion.LimitMaxPosition);
+            //double? result = dialogService.ShowEditNumDialog(0, SelectedMotion.LimitMinPosition, SelectedMotion.LimitMaxSpeed);
+            //if (result == null) return;
+
+            //new DMotionPosition
+            //{
+            //    Name = name.Name,
+            //    Position = result.Value,
+            //    Speed = 
+            //};
+
+            //SelectedMotion.PositionList.Add();
         }
         #endregion
     }
