dbdaeee9dfcd4fb2442f6af02048de26cdc82b79
sean.kim <51458279+gony0811@users.noreply.github.com>
2025-12-30 16:13:24 +0900

    IO Page 수정

--- src/HCB.UI/MAIN UI/ViewModels/USub07ViewModel.cs
+++ src/HCB.UI/MAIN UI/ViewModels/USub07ViewModel.cs
@@ -1,91 +1,66 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HCB.Data.Entity.Type;
+using HCB.Data.Repository;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HCB.UI
 {
     public partial class USub07ViewModel : ObservableObject 
     {
+        private readonly IoDataRepository ioRepository;
         [ObservableProperty]
-        private ObservableCollection<SensorIoItemViewModel> inputIoItems = new ObservableCollection<SensorIoItemViewModel>();
+        private ObservableCollection<SensorIoItemViewModel> analogInput = new ObservableCollection<SensorIoItemViewModel>();
 
         [ObservableProperty]
-        private ObservableCollection<SensorIoItemViewModel> outputIoItems = new ObservableCollection<SensorIoItemViewModel>();
+        private ObservableCollection<SensorIoItemViewModel> analogOutput= new ObservableCollection<SensorIoItemViewModel>();
 
-        public USub07ViewModel()
+        [ObservableProperty]
+        private ObservableCollection<SensorIoItemViewModel> digitalInput= new ObservableCollection<SensorIoItemViewModel>();
+
+        [ObservableProperty]
+        private ObservableCollection<SensorIoItemViewModel> digitalOutput = new ObservableCollection<SensorIoItemViewModel>();
+
+        public USub07ViewModel(IoDataRepository ioDataRepository)
         {
-            // Sensor Item 정리
-            // 입력 신호 샘플  
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-            InputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: true));
-
-
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
-            OutputIoItems.Add(new SensorIoItemViewModel("센서 G", isChecked: true, isReadOnly: false));
+            this.ioRepository = ioDataRepository;
+            _ = LoadIoData();
         }
+
+        public async Task LoadIoData()
+        {
+            var ioList = await ioRepository.ListAsync(x => x.IsEnabled);
+
+            AnalogInput.Clear();
+            AnalogOutput.Clear();
+            DigitalInput.Clear();
+            DigitalOutput.Clear();
+
+            foreach (var group in ioList.GroupBy(x => x.IoDataType))
+            {
+                foreach (var io in group)
+                {
+                    
+
+                    switch (group.Key)
+                    {
+                        case IoType.AnalogInput:
+                            AnalogInput.Add(new SensorIoItemViewModel(io.Name, false, true));
+                            break;
+                        case IoType.AnalogOutput:
+                            AnalogOutput.Add(new SensorIoItemViewModel(io.Name));
+                            break;
+                        case IoType.DigitalInput:
+                            DigitalInput.Add(new SensorIoItemViewModel(io.Name, false, true));
+                            break;
+                        case IoType.DigitalOutput:
+                            DigitalOutput.Add(new SensorIoItemViewModel(io.Name));
+                            break;
+                    }
+                }
+            }
+        }
+
     }
 }
